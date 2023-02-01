de02617ba8d8afa01196b7844515299a4d9d9037
piero01 <pierandreaparmentola@gmail.com>
2023-02-01 13:51:19 +0100

    Initial commit

---
+++ src/main/java/it/unimol/val_backend/ValBackendApplication.java
@@ -0,0 +1,13 @@
+package it.unimol.val_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValBackendApplication.class, args);
+	}
+
+}
