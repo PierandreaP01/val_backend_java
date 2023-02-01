0956be56046d9f7cea12363c94679e3961b5321e
piero01 <pierandreaparmentola@gmail.com>
2023-02-01 21:04:01 +0100

    Fixed bug surefire with tests

--- src/test/java/it/unimol/val_backend/ValBackendApplicationTests.java
+++ src/test/java/it/unimol/val_backend/ValBackendApplicationTests.java
@@ -1,13 +1,13 @@
 package it.unimol.val_backend;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ValBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
